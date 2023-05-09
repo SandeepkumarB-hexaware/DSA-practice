7243abe2e23dd49d0621859bd8b3eec757ad692d
sandeep kumar <sandeepkumarb@hexaware.com>
2023-05-09 16:10:31 +0530

    Create linearSearch.js
    linear search example

---
+++ linearSearch.js
@@ -0,0 +1,10 @@
+function linearSearch(arr,target){
+  for(let i=0; i<arr.length; i++){
+    if(arr[i] == target) return i;
+  }
+  
+  return -1;
+}
+
+console.log(linearSearch([3,5,10,12],10)); // 12
+console.log(linearSearch([3,5,10,12],6)); // -1
