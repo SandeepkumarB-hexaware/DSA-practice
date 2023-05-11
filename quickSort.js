35e3cbcc19e8084dd9c0ef4cb6c896a44fb62446
sandeep kumar <sandeepkumarb@hexaware.com>
2023-05-11 17:15:03 +0530

    Create quickSort.js

---
+++ quickSort.js
@@ -0,0 +1,21 @@
+function quickSort(arr){
+
+ if(arr.length < 1) return arr;
+ 
+ let left = [];
+ let right = [];
+ let pivot = arr[arr.length-1]; // taking last element as a pivot
+ 
+ for(let i=0;i<arr.length-1;i++){
+   if(arr[i] < pivot){
+     left.push(arr[i])
+   }else{
+     right.push(arr[i]);
+   }
+ }
+ 
+ return [...quickSort(left),pivot,...right];
+  
+}
+
+console.log(quickSort([20,-8,6,-2,3])); // [ -8, -2, 3, 20, 6 ]
