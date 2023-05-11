9ea63cd1e4f1032536c4577a25ae13efd2527390
sandeep kumar <sandeepkumarb@hexaware.com>
2023-05-11 15:20:57 +0530

    Create bubbleSort.js

---
+++ bubbleSort.js
@@ -0,0 +1,19 @@
+function bubbleSort(arr){
+  let swapped;
+  
+  do {
+    swapped = false;
+    for(let i=0; i< arr.length -1; i++){
+      if(arr[i] > arr[i+1]){
+        let temp = arr[i];
+        arr[i] = arr[i+1];
+        arr[i+1] = temp;
+        swapped = true;
+      }
+    }
+  }while(swapped)
+  
+  return arr;
+}
+
+console.log(bubbleSort([20,-8,6,-2,3])); // [ -8, -2, 3, 6, 20 ]
