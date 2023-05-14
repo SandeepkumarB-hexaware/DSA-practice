a07db1a9d2e524aabc75851229d96d894c87fc47
sandeep kumar <sandeepkumarb@hexaware.com>
2023-05-14 12:42:21 +0530

    Update cartesianProduct.js

--- cartesianProduct.js
+++ cartesianProduct.js
@@ -10,4 +10,4 @@ function cartesianProduct(arr1,arr2){
   return result;
 }
 
-console.log(cartesianProduct([1,2],[3,4])); //[ [ 1, 3 ], [ 1, 4 ], [ 2, 3 ], [ 2, 4 ] ]
+console.log(cartesianProduct([1,2],[3,4])); //[ [ 1, 3 ], [ 1, 4 ], [ 2, 3 ], [ 2, 4 ] ] O(n) = O(mn)
